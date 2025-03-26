aee584e50ec52d6bbf405ca3efa0ef519594a8ad
Sounak-Hazra <hazrasounak87@gmail.com>
2025-03-26 13:43:17 +0530

    delete test

--- src/app/(app)/test/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FullScreenphoto from '@/app/components/(utility)/FullScreenPhoto'
-const page = () => {
-    return (
-      <FullScreenphoto setIsFullScreen = {null} imgSrc="./myPhotos/myPhoto1.jpg" />
-  )
-}
-
-export default page
